2f382c8e05904be0b40d904463a92f505e85ca9f
RoomTemperatureIQ <37945076+RoomTemperatureIQ@users.noreply.github.com>
2018-10-07 19:21:12 -0500

    Update firstrun.sh

--- scripts/firstrun.sh
+++ scripts/firstrun.sh
@@ -2,9 +2,15 @@
 
 rm /etc/ssh/ssh_host_*
 dpkg-reconfigure openssh-server
-dpkg-reconfigure ssh
 systemctl restart openssh-server
-systemctl restart ssh
+
+# sync NTP: https://askubuntu.com/questions/254826/how-to-force-a-clock-update-using-ntp/254846#254846
+( /etc/init.d/ntp stop
+until ping -nq -c3 8.8.8.8; do
+   echo "Waiting for network..."
+done
+ntpdate -s time.nist.gov
+/etc/init.d/ntp start )&
 
 apt-get update
 apt-get install locate netselect-apt
