33bd5b701fe3cca80ff1f64f1960248c8390e845
RoomTemperatureIQ <37945076+RoomTemperatureIQ@users.noreply.github.com>
2018-10-13 22:45:08 -0500

    Update hostapd-quidnunc.sh

--- usr/local/bin/hostapd-quidnunc.sh
+++ usr/local/bin/hostapd-quidnunc.sh
@@ -1,14 +1,12 @@
 #!/bin/sh
-# placed under /usr/local/bin so user `hostapd_cli` can execute
+# placed under /usr/local/bin so user `hostapd` can execute
 # chmod +x hostapd-quidnunc.sh
-logger -t $0 "hostapd event received $1 $2 $3"
+logger -t "$0" "hostapd event received $1 $2 $3"
 
-if [[ $2 == "AP-STA-CONNECTED" ]]
-then
-  echo "someone has connected with mac id $3 on $1"
+if [ "$2" = "AP-STA-CONNECTED" ]; then
+  echo "someone has connected with mac id $3 on $1" >> /var/log/hostapd-quidnunc.log
 fi
 
-if [[ $2 == "AP-STA-DISCONNECTED" ]]
-then
-  echo "someone has disconnected with mac id $3 on $1"
+if [ "$2" = "AP-STA-DISCONNECTED" ]; then
+  echo "someone has disconnected with mac id $3 on $1" >> /var/log/hostapd-quidnunc.log
 fi
