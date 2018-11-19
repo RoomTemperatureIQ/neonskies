0b4a4f038637f181a460469bcb78ceca025d8e3e
RoomTemperatureIQ <37945076+RoomTemperatureIQ@users.noreply.github.com>
2018-11-19 03:21:09 -0600

    Update ipset_bogons.sh

--- scripts/ipset_bogons.sh
+++ scripts/ipset_bogons.sh
@@ -17,15 +17,25 @@
 
 target="http://www.team-cymru.org/Services/Bogons/fullbogons-ipv4.txt"
 # Alternative source: https://files.pfsense.org/lists/fullbogons-ipv4.txt
+target2="https://files.pfsense.org/lists/fullbogons-ipv4.txt"
 ipset_params="hash:net"
 
 filename=$(basename ${target})
 firewall_ipset=${filename%.*}           # ipset will be filename minus ext
 data_dir="/var/tmp/${firewall_ipset}"   # data directory will be same
+temp_data_dir1="/var/tmp/${firewall_ipset}"   # data directory will be same
 data_file="${data_dir}/${filename}"
+
+filename2=$(basename ${target2})
+firewall_ipset2=${filename2%.*}           # ipset will be filename minus ext
+data_dir2="/var/tmp/${firewall_ipset2}"   # data directory will be same
+temp_data_dir2="/var/tmp/${firewall_ipset2}_temp2"   # data directory will be same
+data_file2="${data_dir2}/${filename2}"
 
 # if data directory does not exist, create it
 mkdir -pm 0750 ${data_dir}
+mkdir -pm 0750 ${temp_data_dir1}
+mkdir -pm 0750 ${temp_data_dir2}
 
 # function to get modification time of the file in log-friendly format
 get_timestamp() {
@@ -34,9 +44,11 @@ get_timestamp() {
 
 # file modification time on server is preserved during wget download
 [ -w ${data_file} ] && old_timestamp=$(get_timestamp ${data_file})
+[ -w ${data_file2} ] && old_timestamp2=$(get_timestamp ${data_file2})
 
 # fetch file only if newer than the version we already have
-wget -qNP ${data_dir} ${target}
+wget -qNP ${temp_data_dir} ${target}
+wget -qNP ${temp_data_dir2} ${target2}
 
 if [ "$?" -ne "0" ]; then
     logger -p cron.err "IPSet: ${firewall_ipset} wget failed."
@@ -44,10 +56,14 @@ if [ "$?" -ne "0" ]; then
 fi
 
 timestamp=$(get_timestamp ${data_file})
+timestamp2=$(get_timestamp ${data_file2})
 
 # compare timestamps because wget returns success even if no newer file
-if [ "${timestamp}" != "${old_timestamp}" ]; then
+if [ "${timestamp}" != "${old_timestamp}" || "${timestamp2}" != "${old_timestamp2}" ]; then
 
+    cat ${temp_data_dir}/${filename} ${temp_data_dir2}/${filename2} > ${data_file}
+    sort -u ${data_file}
+    
     temp_ipset="${firewall_ipset}_temp"
     ipset create ${temp_ipset} ${ipset_params}
 
