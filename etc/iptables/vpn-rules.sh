b68512823ae400833feeb3e6d1cf8e1a918eea29
RoomTemperatureIQ <37945076+RoomTemperatureIQ@users.noreply.github.com>
2018-11-18 22:26:35 -0600

    Update vpn-rules.sh

--- etc/iptables/vpn-rules.sh
+++ etc/iptables/vpn-rules.sh
@@ -301,10 +301,11 @@ $IPT -t nat -N LOGMASQUERADE-NAT
 $IPT -t nat -I LOGMASQUERADE-NAT -j LOG --log-prefix "IPTables-NAT-Masqueraded: " --log-level 4
 $IPT -t nat -A LOGMASQUERADE-NAT -j MASQUERADE
 
-$IPT -t nat -A PREROUTING -p udp --dport 53 -i $LAN_NIC -j DNAT --to-destination $LAN_SERVER_IP
-$IPT -t nat -A PREROUTING -p tcp --dport 53 -i $LAN_NIC -j DNAT --to-destination $LAN_SERVER_IP
-$IPT -t nat -A PREROUTING -p udp --dport 53 -i $WLAN_NIC -j DNAT --to-destination $WLAN_SERVER_IP
-$IPT -t nat -A PREROUTING -p tcp --dport 53 -i $WLAN_NIC -j DNAT --to-destination $WLAN_SERVER_IP
+### DNAT if port 53 is not for interface IP
+$IPT -t nat -A PREROUTING -p udp --dport 53 -i $LAN_NIC -d ! $LAN_SERVER_IP -j DNAT --to-destination $LAN_SERVER_IP
+$IPT -t nat -A PREROUTING -p tcp --dport 53 -i $LAN_NIC -d ! $LAN_SERVER_IP -j DNAT --to-destination $LAN_SERVER_IP
+$IPT -t nat -A PREROUTING -p udp --dport 53 -i $WLAN_NIC -d ! $WLAN_SERVER_IP -j DNAT --to-destination $WLAN_SERVER_IP
+$IPT -t nat -A PREROUTING -p tcp --dport 53 -i $WLAN_NIC -d ! $WLAN_SERVER_IP -j DNAT --to-destination $WLAN_SERVER_IP
 # $IPT -t nat -I PREROUTING -j LOGACCEPT-NAT
 
 $IPT -t nat -P INPUT ACCEPT
